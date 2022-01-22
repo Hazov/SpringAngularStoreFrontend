5c7604ac10e164979a40ffda6760e87c41614e87
nikolay <nonaliud@mail.ru>
2022-01-22 23:17:08 +0300

    update pages buttons method

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Injectable,OnInit} from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
 import {SortedProduct} from '../SortedProduct';
 import {ProductService} from '../product.service';
 import {Category} from '../../category/category';
@@ -20,23 +20,33 @@ export class ProductListComponent implements OnInit {
   tabs: number;
   currentPage: number;
   pagesCount: number;
-  pagesNumbers: number[];
+  pagesButtons: any[];
 
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
+  }
 
   ngOnInit(): void {
     this.showAllProductsByCategory(1);
+    this.currentPage = 1;
   }
 
-  showSpecifiedPage(page: number) {
-    let category = this.currentCategory
+  showSpecifiedPage(page: any) {
+    let category = this.currentCategory;
+    if (page === '<') {
+      page = this.currentPage - 1;
+    }
+    if (page === '>') {
+      page = this.currentPage + 1;
+    }
     this.showAllProductsByCategory(page, category);
     this.currentPage = page;
   }
 
-    showAllProductsByCategory(page:number, category?:string){
-      if(category === undefined) category = 'all';
+  showAllProductsByCategory(page: number, category?: string) {
+    if (category === undefined) {
+      category = 'all';
+    }
     this.productService.getAllProductByCategory(page, category).subscribe(data => {
       this.products = data.products;
       this.pagesCount = data.pagesCount;
@@ -45,14 +55,27 @@ export class ProductListComponent implements OnInit {
   }
 
 
-   updatePagesButtons() {
-      this.pagesNumbers = []
-      for (let i = 0; i < this.pagesCount; i++) {
-        this.pagesNumbers[i] = i;
+  updatePagesButtons() {
+    console.log(this.currentPage)
+    this.pagesButtons = [];
+    if (this.pagesCount > 0){
+      if (this.pagesCount < 5) {
+        for (let i = 0; i < this.pagesCount; i++)
+          this.pagesButtons[i] = i;
+      } else {
+        const LAST = this.pagesCount - 1;
+        switch (this.currentPage){
+          case 1: this.pagesButtons = [1, 2, '>', LAST]; break;
+          case 2: this.pagesButtons = [1, this.currentPage, '>', LAST]; break;
+          case this.pagesCount - 1: this.pagesButtons = [1, '<', this.currentPage, LAST]; break;
+          default: this.pagesButtons = [1, '<', this.currentPage, '>', LAST];
+        }
       }
+    }
   }
 
-  setTab(n: number): void{
+
+  setTab(n: number): void {
     this.tabs = n;
   }
 }
